77faf23840a12b4e12761997111081ca94cfd00d
futbol4me <dev.edward.lee@gmail.com>
2024-04-26 14:05:45 -0700

    factored into modules

---
+++ sudo_euler96.py
@@ -0,0 +1,209 @@
+# two classes useful in solving project euler #96
+# https://projecteuler.net/problem=96
+
+import itertools
+
+class Board():
+    ''' defines a Sudoku board (basically an 81-len np.array of digits, with 0 representing empty)
+    '''
+    import numpy as np
+    import itertools
+
+    DIGITS = {1,2,3,4,5,6,7,8,9}
+
+    def __init__(self,board_ndarray):
+        self.board = board_ndarray.flatten()
+        assert(len(self.board)==81)
+
+    def __repr__(self):
+        slist = repr(self.board.reshape(9,9)).replace('0',' ').split('\n')
+        s = ''
+        for n, line in enumerate(slist):
+            s+= line[:15] + ' | ' + line[17:24] + ' | ' + line[26:] + '\n'
+            if n in {2,5,8}:
+                s+='\n'
+        return s
+
+    def unassigned_cells(self):
+        ''' returns set of unassigned cells (delivered as r,c tuples) '''
+        return set((divmod(i,9) for i,n in enumerate(self.board) if n==0))
+
+    def row(self,i):
+        ''' returns a row as an array '''
+        return self.board.reshape(9,9)[i]
+
+    def column(self,j):
+        ''' returns column as an array '''
+        return self.board.reshape(9,9)[:,j]
+
+    def block(self,i,j=None,flatten = True):
+        ''' returns block (0-8) or the block associated with (r,c) as array or 2d array'''
+        if j is None:
+            i,j = divmod(i,3)
+        else:
+            i,j = i//3, j//3
+        sq = self.board.reshape(9,9)[i*3:i*3+3, j*3:j*3+3]
+        return sq.flatten() if flatten else sq
+
+    def count_filled(self):
+        return np.count_nonzero(self.board)
+
+    def calc_domain_at(self, i,j=None):
+        ''' calculate domain at i,j based on its row, column and block neighbors
+        '''
+        if j is None:
+            (i,j)=i
+        if (d:=self.board[i*9+j]):
+            return {d}
+        blockset = set(self.block(i,j)) - {0}
+        rowset = set(self.row(i)) - {0}
+        colset = set(self.column(j)) - {0}
+        neighborset = blockset | colset | rowset  #union of all already assigned numbers
+        return self.DIGITS - neighborset
+
+    def cell(self,r,c):
+        return self.board.reshape(9,9)[r,c]
+
+    def unsolvable(self):
+        ''' return True if board contains empty assignment domains
+        '''
+        valid_domains = all(map(len,
+                       np.array([{n} if n else self.calc_domain_at(*divmod(i,9)) for i,n in enumerate(self.board)])
+                       ))
+        return not valid_domains
+
+    def violates_constraint(self):
+        ''' check if any duplicates in row, col or block - count nonzeroes vs set of items
+        '''
+        for i in range(9):
+            ra = self.row(i)
+            ca = self.column(i)
+            ba = self.block(i)
+            if (
+                np.count_nonzero(ra) - len(set(ra)-{0})
+                ) or (
+                np.count_nonzero(ca) - len(set(ca)-{0})
+                ) or (
+                np.count_nonzero(ca) - len(set(ca)-{0})):
+                return True
+        # all good
+        return False
+
+    def assign(self,cell,digit):
+        ''' assigns cell (r,c) tuple to item
+        '''
+        i,j = cell
+        assert(self.board[i*9+j]==0)
+        self.board[i*9+j]=digit
+
+    def copy(self):
+        '''
+        returns a new board copy
+        '''
+        return Board(np.copy(self.board))
+
+    def neighbors_of(self, i, j):
+        ''' returns set of neighbors (connected to cell via row, column or block)'''
+        n = set(itertools.chain(
+            ((r,j) for r in range(9)),
+            ((i,c) for c in range(9)),
+            (((i//3)*3+r, (j//3)*3+c) for r in range(3) for c in range(3))
+            )
+        )
+        return n-{(i,j)}
+
+    def sort_cells_by_rc(self,cells):
+        ''' takes set of cells or iterable collection and sorts by row, column '''
+        return sorted(cells,key=lambda x:x[0]*9+x[1])
+
+    def sort_cells_by_domain(self,cells):
+        ''' takes set of cells or iterable collection and sorts by domain size '''
+        return sorted(cells,key=lambda x:len(self.calc_domain_at(x[0],x[1])))
+
+    def unassigned_domains(self,sort_domain=True):
+        ''' returns list of tuples of unassigned cells with their corresponding domains'''
+        cells = self.unassigned_cells()
+        if sort_domain:
+            return [((r,c),self.calc_domain_at(r,c)) for r,c in self.sort_cells_by_domain(cells)]
+        return [((r,c),self.calc_domain_at(r,c)) for r,c in self.sort_cells_by_rc(cells)]
+
+    def unassigned_neighbors(self,r,c,sort_domain=True):
+        ''' returns list of tuples of unassigned neighbors with their corresponding domains'''
+        cells = self.unassigned_cells() & self.neighbors_of(r,c)
+        if sort_domain:
+            return [((r,c),self.calc_domain_at(r,c)) for r,c in self.sort_cells_by_domain(cells)]
+        return [((r,c),self.calc_domain_at(r,c)) for r,c in self.sort_cells_by_rc(cells)]
+
+
+class Solver():
+    ''' defines an ai solver for a sudoku board.  All methods here are static...
+        no real reason these are encapsulated in the class other than to keep it organized
+    '''
+
+    @staticmethod
+    def best_unassigned(board,randomize=False):
+        ''' returns most restricted (cell, {domain}).  We use a list to capture all cells with smallest domains in case
+            can randomize our selections; otherwise we provide first item in the list
+        '''
+        sorted_domains = board.unassigned_domains(sort_domain=True)
+        first = sorted_domains[0]
+        if not randomize:
+            return first
+        smallest_domains = list(filter(lambda x:len(x[1])==len(first[1]),sorted_domains))
+        return random.choice(smallest_domains)
+
+    @staticmethod
+    def df_search(board, rand=False):
+        ''' stack implementation of backtracking depth first search for board solutions
+            stack contains a 3-tuple (cell, assignment, board state prior to assignment)
+            think of cell and assignment as edge between board states
+        '''
+
+        history = [(board.copy(),board.count_filled())]
+
+        if board.violates_constraint() or board.unsolvable():
+            # unsolved state; bail out with history
+            return history
+
+        # create empty stack of edges; access via append and pop
+        edges_to_explore = []
+
+        while True:
+            # return board if completed
+            if board.count_filled()==81:
+                return history
+
+            # find new edges to explore:
+            # get the cell with the smallest domains and put those edges on the exploration stack
+            cell, domain = Solver.best_unassigned(board, randomize=rand)
+            for digit in domain:
+                # we store edges as transitions from one state to another
+                edge = (cell, digit, board.copy())
+                edges_to_explore.append(edge)
+
+            # explore our graph pulling candidates from our stack
+            try_another_edge = True
+            while try_another_edge:
+                if not edges_to_explore:
+                    # nothing to explore; couldnt find solution
+                    return history
+
+                cell, value, board = edges_to_explore.pop()
+                # update board state based on the transition
+                board.assign(cell,value)
+                history.append((board, board.count_filled()))
+                # if our assignment results in unsolvable board, we try another edge
+                try_another_edge = board.unsolvable()
+
+
+    @staticmethod
+    def solve(board,history=False,randomize=False):
+        ''' convenience method to call df_search.  Uses history flag to get final solution
+            or return history of intermediate steps
+        '''
+
+        solution = Solver.df_search(board,randomize)
+        if history:
+            return solution
+        else:
+            return solution[-1][0] #just the board
---
+++ sudoviz.py
@@ -0,0 +1,313 @@
+#@markdown functions to break down a sudoku image into its board positions and utilities to display these images
+## set of sudoku image processing ultilities
+## ed lee
+
+from sudo_euler96 import Solver, Board
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import random
+import tensorflow as tf
+
+def load_grayscale(path):
+    img = cv2.imread(path,cv2.IMREAD_GRAYSCALE)
+    assert img is not None, "file could not be read, check with os.path.exists()"
+    return img
+
+def show(grayimg, show_axis=False):
+    plt.imshow(grayimg, cmap='gray', vmin=0, vmax=255)
+    if not show_axis:
+        plt.axis('off')
+    plt.show()
+
+def locate_sudoku_image_from(path, inner=True):
+    ## use cv2 contours
+    ## https://docs.opencv.org/4.x/d4/d73/tutorial_py_contours_begin.html
+    ## https://docs.opencv.org/4.x/d7/d4d/tutorial_py_thresholding.html
+    imc = cv2.imread(path)
+    img = load_grayscale(path)
+    imgb = cv2.GaussianBlur(cv2.cvtColor(imc, cv2.COLOR_BGR2GRAY) , (7, 7), 3)
+    thresh = cv2.bitwise_not(
+                cv2.adaptiveThreshold(cv2.GaussianBlur(imgb, (7, 7), 3),
+                                      255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
+    )
+
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # https://docs.opencv.org/4.x/dc/dcf/tutorial_js_contour_features.html
+    # https://stackoverflow.com/questions/62274412/cv2-approxpolydp-cv2-arclength-how-these-works
+    big_contours = sorted(contours, key = cv2.contourArea, reverse = True)[:5]
+    # loop over the contours
+    for c in big_contours:
+        # approximate the contour
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+        # look for 4 points in our contour
+        if len(approx) == 4:
+            board_contours = approx.reshape(4,2)
+            # check if rougly square
+            # opencv coordinates in x,y not r,c array indexing
+            x1, x2, x3, x4 = sorted(board_contours[:,0])
+            y1, y2, y3, y4 = sorted(board_contours[:,1])
+
+            return i2square(img[y2:y3,x2:x3])
+    # not found
+    assert False, "sudoku board boundaries not found"
+
+
+def i2square(img):
+    ''' transforms any image to square dimensions '''
+    dim = max(img.shape)
+    return cv2.resize(img,(dim,dim),interpolation = cv2.INTER_CUBIC)
+
+def contrast(grayimg):
+    thresh = cv2.threshold(grayimg, 0, 255,
+		cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    return thresh
+
+def clear_gridlines(gray,black_background=True):
+    ''' clears gridline areas of a sudoku to black... assumes background image is black (reversed)
+    '''
+    gray = gray.copy()
+    LINE_PCT = 0.25
+    dim = gray.shape[0]
+    widthf=dim/9
+    ldim = int(dim * LINE_PCT / 10 +.5)
+    hblock = np.zeros((ldim,dim), np.uint8)
+    vblock = hblock.reshape((dim,ldim))
+    gray[:ldim,:] = hblock
+    gray[dim-ldim:,:]=hblock
+    for i in range(1,9):
+        r = int(i*widthf+.5)-(ldim//2)
+        gray[r:r+ldim,:]=hblock
+    gray[:,:ldim] = vblock
+    gray[:,dim-ldim:]=vblock
+    for i in range(1,9):
+        c = int(i*widthf+.5)-(ldim//2)
+        gray[:,c:c+ldim]=vblock
+    return gray
+
+
+def full_square(img, r,c ):
+    ''' takes grayscale image and r,c parameters and returns corresponding
+        section of the image.  may contain borders
+    '''
+    BORDERFACTOR = 0.015
+
+    dim = img.shape[0]
+
+    borderpixels = int(dim * BORDERFACTOR)
+    width = dim - borderpixels
+    boxdim = width/9
+    oneborder = borderpixels // 2
+
+    i = int(oneborder + r * boxdim)
+    j = int(oneborder + c * boxdim)
+
+    return img[i:min(i+int(boxdim+.5),dim), j:min(dim,j+int(boxdim+.5))]
+
+def get_square(img,r,c,shrink=None,jitter=False):
+    ''' gets img square at r,c  shrink reduces the frame of the image, effectively zooming in
+        this calls full_square then cuts away the edges of the image to focus on the center
+    '''
+
+    sq=full_square(img,r,c)
+    if shrink is None:
+        return sq
+
+    if jitter:
+        shrink = shrink + (np.random.rand()-0.5)*.20
+    dim = sq.shape[0]
+    pad = int(dim * shrink / 2)
+    if not jitter:
+        jr, jc = 0,0
+    else:
+        jr = random.randint(-pad//3,pad//3)
+        jc = random.randint(-pad//3,pad//3)
+    padr = pad + jr
+    padc = pad + jc
+    crop = sq[padr:dim-padr,padc:dim-padc]
+    a,b = crop.shape
+    if a==b:
+        return crop
+    return cv2.resize(crop,(min(a,b),min(a,b)))
+
+
+
+def replace_square(img, r,c , value, digits=None):
+    ''' takes greyscale image and rc parameters and replaces part of image with value; returns the
+        mutated image.
+    '''
+    BORDERFACTOR = 0.015
+    SHRINK = .35
+
+    img = img.copy()
+    dim = img.shape[0]
+
+    if digits is None:
+        digits = build_digits_dict()
+
+    bpixels = int(dim * BORDERFACTOR)
+    width = dim - bpixels
+    boxdim = width/9
+    b1 = bpixels // 2
+    shrinkpix = int(boxdim*SHRINK)//2
+
+    i = int(b1 + r * boxdim)
+    j = int(b1 + c * boxdim)
+
+    rl, rh, cl, ch = i + shrinkpix, i+int(boxdim+.5)-shrinkpix, j+shrinkpix, j+int(boxdim+.5)-shrinkpix
+
+
+    cell_dims = rh-rl,ch-cl
+    img[rl:rh, cl:ch] = cv2.resize(digits[value],cell_dims)
+    return img
+
+def build_digits_images()-> dict:
+    ''' utility to build 28x28 digit images used in replace_square
+        ...needs file training0.png
+    '''
+    g = locate_sudoku_image_from(tpath+'training0.png')
+
+    keymap_for_train0 = {
+            1:(1,7), 2:(2,7), 3:(6,3),
+            4:(1,5), 5:(1,6), 6:(1,3),
+            7:(2,6), 8:(2,5), 9:(1,8), 0:(1,4),
+    }
+    return {digit: cv2.resize(
+                            get_square(g, *keymap_for_train0[digit], shrink=.2),
+                            (28,28)
+                            ) for digit in range(10)
+            }
+
+DIGIT_IMAGES = build_digits_images()
+
+
+def color2gray(rgb):
+    return cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
+
+def view28(x):
+    ''' utility function to display 12 rows x whatever columns of keras-formatted training data (shape and magnitude)
+    '''
+    import math
+    n,r,c,_ = x.shape
+    x = x.reshape(n,r,c)
+    cols = int(math.ceil(n/12))
+    img = np.zeros((12 * 30, cols * 30))
+    i = 0
+    for g in x:
+        c,r = divmod(i,12)
+        img[r*30:r*30+28,c*30:c*30+28]=g
+        i += 1
+    show(img)
+
+
+#@markdown functions to build our down training data... currently requires 10 specific training files in training directory.
+
+import sys
+sys.path.insert(0, path[:-1])
+from sudo_train import TRAINING_DB
+#imports training data labels for training images.
+#TRAINING_DB is dict of {filename: list of labels}
+
+def build_data_set(tdict:dict, db:list = [], jitter=False, mypath=False) -> list:
+    '''
+    reads dict of sudoku image files and labels (coded as list of strings per euler96)
+    returns db, which is a list of tuples with np.array image and label
+    this function can be rerun to add  more training data...
+    then this list should be further processed before training
+    '''
+    db=db.copy()
+    for file in tdict:
+        label_list = tdict[file]
+        filepath = fpath if mypath else tpath
+        gray = locate_sudoku_image_from(tpath+file)
+        # invert, high contrast, cleargridlines
+        cgray = clear_gridlines(contrast(gray))
+        for r in range(9):
+            for c in range(9):
+                for i in range(19*jitter+1): #repeat 20x if jitter
+                    label = int(label_list[r][c])
+                    s = cv2.resize(get_square(cgray,r,c,shrink=.2,jitter=jitter),(28,28),interpolation = cv2.INTER_CUBIC)
+                    db.append((s,label))
+                    if label == 0 and i>3:
+                        break  #no need to repeat blanks too often
+    return db
+
+def build_training_set_for_tf(db:list, zeros=True):
+    ''' inputs db from build_data_set and forms to tuple of arrays suitable for tensorflow training
+    '''
+    xtrain = np.array([im for im,label in db if zeros or label])
+
+    d1,d2,d3=xtrain.shape
+    xtrain = xtrain.reshape(d1,d2,d3,1) # add a dimension ??? not sure why; perhaps for bias
+    labels = tf.keras.utils.to_categorical(np.array([label for im,label in db if zeros or label]))
+    return xtrain, labels
+
+
+#@markdown functions that connect imaging processing to sudoku logic
+def read_sudoku_array(impath, model=None, display=True):
+    ''' accepts filename and tensorflow model... gets image and reads into a 9x9 array
+    '''
+
+    if model is None:
+        model=tf.keras.models.load_model(mpath+'sudoku_model_9.keras')
+    gray = locate_sudoku_image_from(impath)
+    if display:
+      show(gray)
+    # slices=find_boundaries(rgb)
+    m=model
+    board = clear_gridlines(contrast(gray))
+    p = []
+    for i in range(9):
+        for j in range(9):
+            s = cv2.resize(get_square(board,i,j,shrink=.2),(28,28),interpolation = cv2.INTER_CUBIC)
+            # if np.mean(s)<5:
+            #     continue
+            d = s.astype('float')
+            d = tf.keras.preprocessing.image.img_to_array(d)
+            p.append(d)
+    pnp = np.array(p)
+    p_array = m.predict(pnp,verbose=0)
+    p=np.argmax(p_array,axis=1).reshape(9,9)
+    return p
+
+def read_sudoku_strlist(imgfile, model=None):
+    ''' covenience function to translate an imagefile into string representation
+    '''
+    a = read_sudoku_array(imgfile, model)
+    s = ''
+    for r in a:
+        for c in r:
+            s+=str(c)
+        s+=','
+    return s[:-1].split(',')
+
+def read_sudoku_board(imgfile,model=None, display=True):
+    ''' reads file into a Board object
+    '''
+    return Board(read_sudoku_array(imgfile,model,display))
+
+def solve_sudoku_image(imgfile, model=None, history=False, randomize=False):
+    initial_board = read_sudoku_board(imgfile,model,False)
+    gray = locate_sudoku_image_from(imgfile)
+    result = Solver.solve(initial_board,history,randomize=randomize)
+    og = gray.copy()
+    if history:
+      solved_board,_ = result[-1]
+    else:
+      solved_board = result
+    diff = (solved_board.board - initial_board.board).reshape(9,9)
+    for i in range(9):
+        for j in range(9):
+            if diff[i,j]:
+                gray = replace_square(gray,i,j,diff[i,j],digit_images)
+
+    dim = og.shape[0]
+    bar = 255-np.zeros((dim,dim//20))
+    show(np.hstack((og,bar,gray)))
+    return result
+
+
+
+
