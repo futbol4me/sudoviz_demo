d9b3fae3c6327738a52ef4872b82ebb4a12be736
Edward Lee <ed@Edwards-MacBook-Air.local>
2024-04-26 21:59:46 -0700

    paths

--- sudoviz.py
+++ sudoviz.py
@@ -209,11 +209,107 @@ def view28(x):
 
 #@markdown functions to build our down training data... currently requires 10 specific training files in training directory.
 
-import sys
-sys.path.insert(0, path[:-1])
-from sudo_train import TRAINING_DB
+# import sys
+# sys.path.insert(0, path[:-1])
+# from sudo_train import TRAINING_DB
 #imports training data labels for training images.
 #TRAINING_DB is dict of {filename: list of labels}
+
+TRAINING_DB={'training1.png': [
+    '263147985',
+    '547893261',
+    '891562473',
+    '136485729',
+    '759621834',
+    '428379156',
+    '385714692',
+    '612958347',
+    '974236518'
+    ],
+    'training2.jpg': [
+    '000000008',
+    '180002300',
+    '060057001',
+    '070960000',
+    '090704010',
+    '000081040',
+    '600240080',
+    '004500093',
+    '500000000'
+    ],
+    'training3.png': [
+    '869135742',
+    '521478936',
+    '347926185',
+    '678341529',
+    '134592867',
+    '952867314',
+    '793284651',
+    '416753298',
+    '285619473'
+    ],
+    'training4.png':[
+    '793864512',
+    '654123897',
+    '128975364',
+    '586219743',
+    '279346185',
+    '341758629',
+    '835491276',
+    '912637458',
+    '467582931'],
+    'training5.png':[
+    '163297458',
+    '275348196',
+    '894651237',
+    '349862571',
+    '751934682',
+    '628175349',
+    '532786914',
+    '486519723',
+    '917423865'],
+    'training6.png':[
+    '406751080',
+    '208000470',
+    '000000906',
+    '530200000',
+    '649130000',
+    '007905100',
+    '070829050',
+    '062010038',
+    '180007009'],
+    'training7.png':[
+    '672531948',
+    '831649275',
+    '549827631',
+    '157496823',
+    '396218754',
+    '284754169',
+    '763185492',
+    '415962387',
+    '928374516'],
+    'training8.jpg':
+    ['700000000',
+     '001400370',
+     '003062000',
+     '050006980',
+     '104000006',
+     '000003000',
+     '009070001',
+     '000094800',
+     '240010009'],
+    'training9.png':
+    ['487691235',
+     '521348679',
+     '963275184',
+     '278954361',
+     '146783592',
+     '359162748',
+     '812436957',
+     '734519826',
+     '695827413']
+   }
+
 
 def build_data_set(tdict:dict, db:list = [], jitter=False, mypath=False) -> list:
     '''
@@ -260,15 +356,12 @@ def read_sudoku_array(impath, model=None, display=True):
     gray = locate_sudoku_image_from(impath)
     if display:
       show(gray)
-    # slices=find_boundaries(rgb)
     m=model
     board = clear_gridlines(contrast(gray))
     p = []
     for i in range(9):
         for j in range(9):
             s = cv2.resize(get_square(board,i,j,shrink=.2),(28,28),interpolation = cv2.INTER_CUBIC)
-            # if np.mean(s)<5:
-            #     continue
             d = s.astype('float')
             d = tf.keras.preprocessing.image.img_to_array(d)
             p.append(d)
